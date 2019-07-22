af9c080f20d0dcfed80d7f54b17bf36b2370865b
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-07-22 12:34:12 +1000

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+#!/bin/bash
+
+#variables
 THIS_NODE_HOST=k8snode1
+LOGFILE=/tmp/k8snode1.log
+
+#install required tools
+sudo yum install yum-utils device-mapper-persistent-data lvm2 ipset ipvsadm git -y
 
 #hostname
-hostnamectl set-hostname ${THIS_NODE_HOST} && \
-cat hosts >> /etc/hosts
+sudo hostnamectl set-hostname ${THIS_NODE_HOST} && \
+sudo cat hosts >> /etc/hosts
 
 #firewalld
-systemctl stop firewalld && \
-systemctl disable firewalld && \
-setenforce 0 && \
-sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+sudo systemctl stop firewalld && \
+sudo systemctl disable firewalld && \
+sudo setenforce 0 && \
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
 #kernel params
-echo -e "net.bridge.bridge-nf-call-ip6tables = 1
+sudo echo -e "net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 vm.swappiness=0" >> /etc/sysctl.d/k8s.conf && \
-modprobe br_netfilter && \
-sysctl -p /etc/sysctl.d/k8s.conf
+sudo modprobe br_netfilter && \
+sudo sysctl -p /etc/sysctl.d/k8s.conf
 
 #ipvs module
-cat > /etc/sysconfig/modules/ipvs.modules <<EOF
+sudo cat > /etc/sysconfig/modules/ipvs.modules <<EOF
 #!/bin/bash
 modprobe -- ip_vs
 modprobe -- ip_vs_rr
@@ -29,25 +36,23 @@ modprobe -- ip_vs_wrr
 modprobe -- ip_vs_sh
 modprobe -- nf_conntrack_ipv4
 EOF
-chmod 755 /etc/sysconfig/modules/ipvs.modules && \
-bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep -e ip_vs -e nf_conntrack_ipv4
-
-#tools
-yum install yum-utils device-mapper-persistent-data lvm2 ipset ipvsadm -y
+sudo chmod 755 /etc/sysconfig/modules/ipvs.modules && \
+sudo bash /etc/sysconfig/modules/ipvs.modules && \
+sudo lsmod | grep -e ip_vs -e nf_conntrack_ipv4
 
 #install and configure docker
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
-yum makecache fast && \
-yum install -y --setopt=obsoletes=0 docker-ce-18.09.7-3.el7 && \
-systemctl start docker && \
-systemctl enable docker && \
-echo -e "{
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
+sudo yum makecache fast && \
+sudo yum install -y --setopt=obsoletes=0 docker-ce-18.09.7-3.el7 && \
+sudo systemctl start docker && \
+sudo systemctl enable docker && \
+sudo echo -e "{
   \"exec-opts\": [\"native.cgroupdriver=systemd\"]
 }" >> /etc/docker/daemon.json && \
-systemctl restart docker
+sudo systemctl restart docker
 
 #install kubeadm and kubelet
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -57,11 +62,11 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-yum install -y kubelet kubeadm kubectl
+sudo yum install -y kubelet kubeadm kubectl
 
 #swap off
-swapoff -a && \
-sed -i '/ swap / s/^/#/' /etc/fstab
+sudo swapoff -a && \
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
 
 #start kubelet
-systemctl enable kubelet.service
+sudo systemctl enable kubelet.service
