8ac9e7cdebb23d2249a4490918f488747ee8f9f1
Henry Liu <henry.liu@team.telstra.com>
2019-07-27 15:23:04 +1000

    adding aws ec2 ubuntu scripts

---
+++ aws-ec2-UBUNTU 18.04-master.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#KUBERNETES MASTER NODE BUILD FOR AWS EC2 UBUNTU 18.04 LTS
+
+#CHANGE BELOW HOSTNAME
+host_name='master'
+int_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+
+#BEGIN
+sudo hostnamectl set-hostname ${host_name} && \
+echo -e "${int_ip} ${host_name}" | sudo tee -a /etc/hosts && \
+sudo apt-get update -y && \
+sudo apt install docker.io -y && \
+sudo systemctl enable docker && \
+echo -e "{
+  \"exec-opts\": [\"native.cgroupdriver=systemd\"]
+}" | sudo tee -a /etc/docker/daemon.json && \
+sudo systemctl restart docker && \
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add && \
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" -y && \
+sudo apt install kubeadm -y && \
+sudo swapoff -a && \
+echo -e "apiVersion: kubeadm.k8s.io/v1beta2
+kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: ${int_ip}
+  bindPort: 6443
+nodeRegistration:
+  taints:
+  - effect: PreferNoSchedule
+    key: node-role.kubernetes.io/master
+---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+kubernetesVersion: v1.15.0
+networking:
+  podSubnet: 10.244.0.0/16" | sudo tee -a /tmp/kubeadm.yaml && \
+sudo kubeadm init --config /tmp/kubeadm.yaml --ignore-preflight-errors=NumCPU > /home/ubuntu/k8sinit.log && \
+mkdir -p /home/ubuntu/.kube && \
+sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config && \
+sudo chown $(id -u):$(id -g) /home/ubuntu/.kube/config && \
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#END
