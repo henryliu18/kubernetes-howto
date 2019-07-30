20a999dc660327da8b013daf6b84f6289200cd23
Henry Liu <henry.liu@team.telstra.com>
2019-07-31 00:03:29 +1000

    kube-test-container

--- aws-ec2-UBUNTU 18.04-TASK5-deploy-apps.sh
+++ aws-ec2-UBUNTU 18.04-TASK5-deploy-apps.sh
@@ -63,6 +63,79 @@ spec:
           serviceName: tomcat-server
           servicePort: 8080" | tee -a /tmp/tomcat-ingress.yaml && \
 kubectl apply -f /tmp/tomcat-ingress.yaml
+
+#Step 5: Deploy a container for testing Kubernetes, expose to port 80
+#Full description and source code: https://github.com/sverrirab/kube-test-container
+echo -e "apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: kube-test-container
+  name: kube-test-container
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      app: kube-test-container
+  template:
+    metadata:
+      labels:
+        app: kube-test-container
+    spec:
+      containers:
+      - name: kube-test-container
+        image: sverrirab/kube-test-container:v1.2
+        imagePullPolicy: IfNotPresent
+        resources:
+          limits:
+            cpu: 2
+            memory: 200Mi
+          requests:
+            cpu: 100m
+            memory: 10Mi
+        ports:
+        - containerPort: 8000
+        livenessProbe:
+          httpGet:
+            path: /
+            port: 8000
+          initialDelaySeconds: 1
+          periodSeconds: 1
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: kube-test-container
+spec:
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 8000
+  selector:
+    app: kube-test-container
+  type: ClusterIP" | tee -a /tmp/kube-test-container-deployment.yaml && \
+kubectl apply -f /tmp/kube-test-container-deployment.yaml
+
+#Step 6: Create Ingress for kube-test-container
+echo -e "apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: kube-test-container-ingress
+spec:
+  tls:
+  - hosts:
+    - test.birdgg.com
+    secretName: test-birdgg-secret
+  rules:
+  - host: test.birdgg.com
+    http:
+      paths:
+      - path: /
+        backend:
+          serviceName: kube-test-container
+          servicePort: 80" | tee -a /tmp/kube-test-container-ingress.yaml && \
+kubectl apply -f /tmp/kube-test-container-ingress.yaml
+
 #print messages of DNS record changes and Dashboard ui token
 gettoken='kubectl describe -n kube-system secret/$(kubectl -n kube-system get secret | grep kubernetes-dashboard-token|cut -d" " -f1)'
 echo "
